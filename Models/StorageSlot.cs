e08ecb83a127741afb30c5e5a9dc23ccbc0b9d57
khanhphamvn222 <149863024+khanhphamvn222@users.noreply.github.com>
2026-06-24 13:42:44 +0700

    update(storage): add CorpseId to StorageSlot model

--- Models/StorageSlot.cs
+++ Models/StorageSlot.cs
@@ -9,6 +9,7 @@ public class StorageSlot
     public SlotStatus Status { get; set; }
     public double Temp { get; set; }
     public string? CorpseName { get; set; }
+    public int? CorpseId { get; set; }
     public int DaysStored { get; set; }
     public string LastUpdate { get; set; } = "";
     public string? PackageName { get; set; }
