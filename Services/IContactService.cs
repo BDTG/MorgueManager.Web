f1f9a26ddded0238547b4215d10873d9cd46a74b
Simpson-31ev3n <sktt1sonpham@gmail.com>
2026-06-26 00:02:38 +0700

    feat: bo sung phuong thuc dem va thong bao thay doi yeu cau lien he

--- Services/IContactService.cs
+++ Services/IContactService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using MorgueManager.Web.Models;
 
@@ -6,4 +7,8 @@ namespace MorgueManager.Web.Services;
 public interface IContactService
 {
     Task<bool> SubmitContactAsync(ContactModel model);
+    Task<int> GetUnprocessedCountAsync();
+    event Action? OnContactRequestsChanged;
+    void NotifyRequestsChanged();
 }
+
